711f1d9101618137cd4f6af80a4e4f4e7dd6c594
Roujan Mavarani <Mavarani@hotmail.co.uk>
2022-01-28 00:34:49 +0000

    3rd update
    not finished yet need to start on session base and login account

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import render_template, Flask
-from database import *
+from flask import render_template, Flask,request
+from sqlalchemy import *
+
+
+
  
 app = Flask(__name__)
-
+engine= create_engine('sqlite:///data.db')
 @app.route('/')
 def index():
     
     return render_template("Base.html")
 
-@app.route('/SignIn')
-def sign_in ():
+@app.route('/SignIn', methods=['POST', 'GET'])
+def login (username,password):
+  if request.method == 'POST':
+        username = request.form.get('Username')
+        password = request.form.get('Password')
+
+        user=User.query.filter_by(username=Username).first()
         return  render_template("SignIn.html")
 
 @app.route('/SignUp')
--- dbinit.py
+++ dbinit.py
@@ -1,13 +1,17 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+
 Base= declarative_base()
-engine = create_engine('sqlite:///User.db', echo = True)
+engine = create_engine('sqlite:///data.db', echo = True)
 
 class table(Base):
         __tablename__='User'
         Id = Column(Integer, primary_key=True, autoincrement=True)
-        Username = Column(String(100),ForeignKey('userInfo'),unique=True,nullable=False)
+        Username = Column(String(100),unique=True,nullable=False)
         Password = Column(String(100), nullable=False)
         Name= Column(String(100), nullable=False)
         Surname= Column(String(100), nullable=False)
@@ -27,6 +31,9 @@ class table(Base):
         
         
         Base.metadata.create_all(bind=engine)
+
+
+
 
   
     
